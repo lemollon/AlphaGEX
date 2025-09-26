96c0823eea57419c5522059d3500db5a5254c85d
Autobot <shairan2016@gmail.com>
2025-09-26 07:40:43 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -7,7 +7,5 @@ Core functionality for the GammaHunter trading system.
 __version__ = "1.0.0"
 __author__ = "GammaHunter Team"
 
-# Import key components for easy access
-from .logger import logger, log_error, log_info, log_warning, log_success
-
-__all__ = ['logger', 'log_error', 'log_info', 'log_warning', 'log_success']
+# Note: Imports are done in the modules that need them
+# to avoid circular import issues during initialization
