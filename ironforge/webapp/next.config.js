e2f2613302fbc4a478f95f089faf01f9c26f78d7
Leron Mollon <shairan2016@gmail.com>
2026-05-06 22:10:31 -0500

    Revert "Merge claude/landing-rewrites-2026-05-06: /landing rewrite fix"
    This reverts commit 48a4ef127fd617d65673e88cdf56d486053df2a4, reversing
    changes made to 7f4cdaf30c5576d79b6c24b6cbe78d00cd2003cc.

--- ironforge/webapp/next.config.js
+++ ironforge/webapp/next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  async rewrites() {
-    return [
-      { source: '/landing', destination: '/landing/index.html' },
-      { source: '/landing/', destination: '/landing/index.html' },
-    ]
-  },
 }
 
 module.exports = nextConfig
